d9fcac00787e7a81a04494a8db08ae65a0e6c8c4
Nic <nic.dan.costea@gmail.com>
2022-10-05 17:27:58 +0200

    Updated version
    - restructured the html in order to render js results
    - DO NOT KNOW how to render what I want [in the index.js file]

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const btn = document.querySelector(".click-btn");
-btn.textContent = "Click me to see which side button wins";
+const homeworkPart1 = document.querySelector("#partOne");
+homeworkPart1.textContent = `${sortArray}`;
+const homeworkPart2 = document.querySelector("#partTwo");
+homeworkPart2.textContent = "Part2";
+
+let n1 = 23;
+let n2 = -3446;
+let n3 = 92329029952;
+let sortArray = [n1, n2, n3];
+sortArray.sort((a, b) => a - b);
+console.log({ sortArray });
